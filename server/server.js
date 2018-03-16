105092e65327c7de9b89b3ecc2191a092dfaefb7
Thiện Nguyễn <girlisbinarycode000@gmail.com>
2018-03-16 15:51:28 +0700

    Set up heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {Todo} = require('./model/todo.js');
 const {User} = require('./model/user.js');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
