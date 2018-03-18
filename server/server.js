9f263554c7802f6ff469e3d2eba2dd68cdf7cab6
Thiện Nguyễn <girlisbinarycode000@gmail.com>
2018-03-18 21:22:34 +0700

    Login API, generate token

--- server/server.js
+++ server/server.js
@@ -104,7 +104,7 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
-//User Login API
+//User API
 app.post('/users', (req, res) => {
   let body = _.pick(req.body, ['email', 'password']);
   let user = new User(body);
@@ -117,6 +117,18 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+// Login API
+app.post('/users/login', (req,res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then(user =>{
+      return user.generateAuthToken()
+        .then(token => res.status(200).header('x-auth', token).send(user))
+    })
+    .catch(e => res.status(400).send());
+})
 
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
